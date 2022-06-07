455a237bea87d2820d1071cb014496f9ef3ce11e
Pauliukovich Artsiom <artem.pavl@protonmail.com>
2022-06-07 17:57:21 +0300

    start work

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { access, constants, writeFile } from 'fs';
+
+const fileName = 'fresh.txt';
+const pathName = `./files/${fileName}`;
+
 export const create = async () => {
-    // Write your code here 
+  access(pathName, constants.F_OK, (err) => {
+      console.log(err);
+    if (err) {
+      writeFile('./src/fs/files/fresh.txt', 'I am fresh and young', (err) => {
+        if (err) {
+          throw new Error(err.message);
+        }
+      });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
+
+create();
